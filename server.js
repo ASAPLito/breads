712907d2fb96741023de07786777dd264916abc1
ASAPLito <micbraner@gmail.com>
2022-06-15 18:56:05 -0400

    Sending index and show data

--- server.js
+++ server.js
@@ -15,9 +15,16 @@ const breadsController = require('./controllers/breads_controller.js');
 app.use('/breads', breadsController);
 
 //MIDDELWARE
+app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+
+
+//404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 //LISTEN
 app.listen(PORT, () => {
